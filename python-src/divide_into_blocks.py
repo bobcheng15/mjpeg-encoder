351037032106cc8a3487c481c931474421291bc4
s106062221 <gaorda310130@gmail.com>
2021-06-21 15:46:13 +0800

    fix the shape of the matrix

--- python-src/divide_into_blocks.py
+++ python-src/divide_into_blocks.py
@@ -5,16 +5,22 @@ im = Image.open('frame0.bmp')
 block_width = int(im.width / 8)
 block_height = int(im.height / 8)
 rgb_pixels = list(im.getdata())
+print(block_width)
+print(block_height)
 
 temp = []
+block = []
 blocks = []
 for i in range(block_height):
 	for j in range(block_width):
 		for m in range(8):
-			address = (i*8+m)*im.width + (j*8)
-			temp.append(rgb_pixels[address: address+8])
-	blocks.append(temp)
-	temp = []
+			for n in range(8):
+				address = (i*8+m)*im.width + (j*8) + n
+				temp.append(rgb_pixels[address])
+		block.append(temp)
+		temp = []
+	blocks.append(block)
+	block = []
 
 
 print(np.shape(blocks))
--- python-src/main.py
+++ python-src/main.py
@@ -7,28 +7,31 @@ block_height = int(im.height / 8)
 rgb_pixels = list(im.getdata())
 
 temp = []
+block = []
 blocks = []
 for i in range(block_height):
 	for j in range(block_width):
 		for m in range(8):
-			address = (i*8+m)*im.width + (j*8)
-			temp.append(rgb_pixels[address: address+8])
-	blocks.append(temp)
-	temp = []
+			for n in range(8):
+				address = (i*8+m)*im.width + (j*8) + n
+				temp.append(rgb_pixels[address])
+		block.append(temp)
+		temp = []
+	blocks.append(block)
+	block = []
 
 yuv_blocks = []
 yuv_block = []
-yuv_row = []
+yuv_pixels = []
 for block in blocks:
-	for row in block:
-		for pixel in row:
-			yuv_pixel = []
+	for pixels in block:
+		for pixel in pixels:
 			transfrom_metrix = [[0.257, 0.504, 0.098], [-0.148, -0.291, 0.439], [0.439, -0.368, -0.071]]
 			yuv = np.dot(transfrom_metrix, pixel) + [16, 128, 128]
-			yuv_row.append(yuv)
-		yuv_block.append(yuv_row)
-		yuv_row = []
+			yuv_pixels.append(yuv)
+		yuv_block.append(yuv_pixels)
+		yuv_pixels = []
 	yuv_blocks.append(yuv_block)
 	yuv_block = []
 
-return yuv_blocks
+print(np.shape(yuv_blocks))
