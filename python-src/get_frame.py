bb8a9992407847a88a47533a27ae4b06e61f6f5e
ShihYT <gaorda310130@gmail.com>
2021-06-22 21:42:13 +0800

    Update get_frame.py

--- python-src/get_frame.py
+++ python-src/get_frame.py
@@ -3,7 +3,7 @@ vidcap = cv2.VideoCapture('big_buck_bunny_720p_5mb.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  cv2.imwrite("frame%d.bmp" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
   count += 1
 
