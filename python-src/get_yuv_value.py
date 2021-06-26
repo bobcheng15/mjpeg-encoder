ea9f1521fdf1cccda273c908b9acf067346bca5c
s106062221 <gaorda310130@gmail.com>
2021-06-26 15:13:10 +0800

    directly use the yuv value from image

--- python-src/get_yuv_value.py
+++ python-src/get_yuv_value.py
@@ -1,18 +1,13 @@
 from PIL import Image
 import numpy as np
 
-im = Image.open('frame0.bmp')
-rgb_pixels = list(im.getdata())
+image = Image.open('frame0.bmp')
+img_yuv = image.convert('YCbCr')
+yuv_pixels = np.ndarray((image.size[1], image.size[0], 3), 'u1', img_yuv.tobytes())
+# print(yuv_pixels.shape)
 
-#r_pixels   = list(im.getdata(band = 0))
-#g_pixels   = list(im.getdata(band = 1))
-#b_pixels   = list(im.getdata(band = 2))
-
-yuv_pixels = []
-transfrom_metrix = [[0.257, 0.504, 0.098], [-0.148, -0.291, 0.439], [0.439, -0.368, -0.071]]
-for pixel in rgb_pixels:
-	yuv = np.dot(transfrom_metrix, pixel) + [16, 128, 128]
-	yuv_pixels.append(yuv)
-
-print(rgb_pixels[:10])
-print(yuv_pixels[:10])
+yuv_block = []
+for pixel in yuv_pixels:
+	yuv_block.append(pixel)
+	
+# print(yuv_block[719][1279][0])
--- python-src/main.py
+++ python-src/main.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import numpy as np
 
-im = Image.open('frame0.bmp')
-block_width = int(im.width / 8)
-block_height = int(im.height / 8)
-rgb_pixels = list(im.getdata())
+image = Image.open('frame0.bmp')
+block_width = int(image.width / 8)    #160
+block_height = int(image.height / 8)  #90
+
+img_yuv = image.convert('YCbCr')
+yuv_pixels = np.ndarray((image.size[1], image.size[0], 3), 'u1', img_yuv.tobytes())
 
 temp = []
 block = []
@@ -13,25 +15,28 @@ for i in range(block_height):
 	for j in range(block_width):
 		for m in range(8):
 			for n in range(8):
-				address = (i*8+m)*im.width + (j*8) + n
-				temp.append(rgb_pixels[address])
+				temp.append(yuv_pixels[i*8+m][j*8+n])
 		block.append(temp)
 		temp = []
 	blocks.append(block)
 	block = []
+	
+print(np.shape(blocks))
+print(blocks[0][0])
 
-yuv_blocks = []
-yuv_block = []
-yuv_pixels = []
-for block in blocks:
-	for pixels in block:
-		for pixel in pixels:
-			transfrom_metrix = [[0.257, 0.504, 0.098], [-0.148, -0.291, 0.439], [0.439, -0.368, -0.071]]
-			yuv = np.dot(transfrom_metrix, pixel) + [16, 128, 128]
-			yuv_pixels.append(yuv)
-		yuv_block.append(yuv_pixels)
-		yuv_pixels = []
-	yuv_blocks.append(yuv_block)
-	yuv_block = []
 
-print(np.shape(yuv_blocks))
+# yuv_blocks = []
+# yuv_block = []
+# yuv_pixels = []
+# for block in blocks:
+	# for pixels in block:
+		# for pixel in pixels:
+			# transfrom_metrix = [[0.257, 0.504, 0.098], [-0.148, -0.291, 0.439], [0.439, -0.368, -0.071]]
+			# yuv = np.dot(transfrom_metrix, pixel) + [16, 128, 128]
+			# yuv_pixels.append(yuv)
+		# yuv_block.append(yuv_pixels)
+		# yuv_pixels = []
+	# yuv_blocks.append(yuv_block)
+	# yuv_block = []
+
+# print(np.shape(yuv_blocks))
